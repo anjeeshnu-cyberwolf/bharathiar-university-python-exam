3a7262bc9973449a960791480ff8271dd9dd184b
anjeeshnu-cyberwolf <114555611+anjeeshnu-cyberwolf@users.noreply.github.com>
2022-09-28 16:28:16 +0530

    Add files via upload

---
+++ anjeeshnubanerjeeq2.py
@@ -0,0 +1,6 @@
+print("A person bought a mobile for the cost of 12,300 with additionally he pays 18% of GST.")
+price=12300
+GST=price*(18/100)
+NetPrice=price+GST
+print("GST",GST)
+print("NetPrice:",NetPrice)
